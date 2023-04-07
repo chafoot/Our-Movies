1f98a59c23997b2a3b65a0a45151d8ee0b4b56b9
Arun Kumar <46559817+chafoot@users.noreply.github.com>
2023-04-07 09:49:40 +0530

    ⚙️updated 07-04-2023

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1115,7 +1115,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ]
             reply_markup = InlineKeyboardMarkup(buttons)
             await query.message.edit_reply_markup(reply_markup)
-    await query.answer('♥️ Thank You LazyDeveloper ♥️')
+    await query.answer('♥️ Thank You ArunArudra ♥️')
 
 async def auto_filter(client, msg, spoll=False):
     if not spoll:
